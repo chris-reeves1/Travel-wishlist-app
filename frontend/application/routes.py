d164191599db0d5c1117c692abe8971be327c746
Ubuntu <chris@dev-jenkins.krq4w0eqge4erlfup50gztqk0b.ax.internal.cloudapp.net>
2021-12-02 09:30:12 +0000

    update1

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -3,10 +3,12 @@ from application.forms import CountryForm
 from flask import render_template, request, redirect, url_for, jsonify
 import requests
 
+backend_host = "wishlist-app_backend:5000"
+
 @app.route('/')
 @app.route('/home')
 def home():
-    all_countries = requests.get("http://wishlist-app_backend:5000/read/allCountries").json()
+    all_countries = requests.get(f"http://{backend_host}/read/allCountries").json()
     app.logger.info(f"Country: {all_countries}")
     return render_template('index.html', title="Home", all_countries=all_countries["country"])
 
@@ -16,7 +18,7 @@ def create_country():
     form = CountryForm()
 
     if request.method == "POST":
-        response = requests.post("http://wishlist-app_backend:5000/create/country",
+        response = requests.post(f"http://{backend_host}/create/country",
         json={"country_name": form.country_name.data})
         return redirect(url_for('home'))
 
@@ -25,32 +27,31 @@ def create_country():
 @app.route('/update/country/<int:id>', methods=['GET','POST'])
 def update_country(id):
     form = CountryForm()
-    country = requests.get(f"http://wishlist-app_backend:5000/update/country/{id}").json()
+    country = requests.get(f"http://{backend_host}/update/country/{id}").json()
     app.logger.info(f"Country: {country}")
 
     if request.method == "POST":
         response = requests.put(
-            f"http://wishlist-app_backend:5000/update/country/{id}",
-            json={"country_name": form.country_name.data}
-        )
+            f"http://{backend_host}/update/country/{id}",
+            json={"country_name": form.country_name.data})
         return redirect(url_for('home'))
 
     return render_template('update_country.html', country=country, form=form)
 
 @app.route('/delete/country/<int:id>')
 def delete_country(id):
-    response = requests.delete(f"http://wishlist-app_backend:5000/delete/country/{id}")
+    response = requests.delete(f"http://{backend_host}/delete/country/{id}")
     app.logger.info(f"Response: {response.text}")
     return redirect(url_for('home'))
 
 @app.route('/visited/country/<int:id>')
 def visited_country(id):
-    response = requests.put(f"http://wishlist-app_backend:5000/visited/country/{id}")
+    response = requests.put(f"http://{backend_host}/visited/country/{id}")
     app.logger.info(f"Response: {response.text}")
     return redirect(url_for('home'))
 
 @app.route('/unvisited/country/<int:id>')
 def unvisited_country(id):
-    response = requests.put(f"http://wishlist-app_backend:5000/unvisited/country/{id}")
+    response = requests.put(f"http://{backend_host}/unvisited/country/{id}")
     app.logger.info(f"Response: {response.text}")
     return redirect(url_for('home'))
