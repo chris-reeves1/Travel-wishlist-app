fd97a197abd694914d6bfc3abf8a6feebb9c9509
Ubuntu <chris@dev-jenkins.krq4w0eqge4erlfup50gztqk0b.ax.internal.cloudapp.net>
2021-12-03 12:27:24 +0000

    tests1

--- frontend/tests/test_unit.py
+++ frontend/tests/test_unit.py
@@ -1,10 +1,19 @@
 from flask import url_for
 from flask_testing import TestCase
 from application import app
+from application.routes import backend_host
+import requests_mock
+
+test_country = {
+                "id": 1,
+                "country_name": "test country",
+                "visited": False
+            }
 
 class TestBase(TestCase):
 
     def create_app(self):
+        # Defines the flask object's configuration for the unit tests
         app.config.update(
             DEBUG=True,
             WTF_CSRF_ENABLED=False
@@ -12,62 +21,96 @@ class TestBase(TestCase):
         return app
 
 class TestViews(TestBase):
+    # Test whether we get a successful response from our routes
     def test_home_get(self):
-        response = self.client.get(url_for('home'))
-        self.assert200(response)
+        with requests_mock.Mocker() as m:
+            all_countries = { "country": [test_country] }
+            m.get(f"http://{backend_host}/read/allCountries", json=all_tasks)
+            response = self.client.get(url_for('home'))
+            self.assert200(response)
     
     def test_create_country_get(self):
         response = self.client.get(url_for('create_country'))
         self.assert200(response)
 
-    def test_read_countries_get(self):
-        response = self.client.get(url_for('read_countries'))
-        self.assert200(response)
-
     def test_update_country_get(self):
-        response = self.client.get(url_for('update_country', id=1))
-        self.assert200(response)
+        with requests_mock.Mocker() as m:
+            m.get(f"http://{backend_host}/read/country/1", json=test_task)
+            response = self.client.get(url_for('update_country', id=1))
+            self.assert200(response)
 
 class TestRead(TestBase):
 
     def test_read_home_countries(self):
-        response = self.client.get(url_for('home'))
-        self.assertIn(b"Run unit tests", response.data)
+        with requests_mock.Mocker() as m:
+            all_countries = { "country": [test_country] }
+            m.get(f"http://{backend_host}/read/allCountries", json=all_tasks)
+            response = self.client.get(url_for('home'))
+            self.assertIn(b"Test the frontend", response.data)
 
 class TestCreate(TestBase):
 
     def test_create_country(self):
-        response = self.client.post(
-            url_for('create_country'),
-            data={"country_name": "Testing create functionality"},
-            follow_redirects=True
-        )
-        self.assertIn(b"Testing create functionality", response.data)
+        with requests_mock.Mocker() as m:
+            all_countries = { "country": 
+                [
+                    test_country,
+                    {
+                        "id": 2,
+                        "country_name": "Testing create functionality",
+                        "visited": False
+                    }
+                ] 
+            }
+            m.post(f"http://{backend_host}/create/country", text="Test response")
+            m.get(f"http://{backend_host}/read/allCountries", json=all_tasks)
+            response = self.client.post(
+                url_for('create_country'),
+                json={"country_name": "Testing create functionality"},
+                follow_redirects=True
+            )
+            self.assertIn(b"Testing create functionality", response.data)
     
 class TestUpdate(TestBase):
 
     def test_update_country(self):
-        response = self.client.post(
-            url_for('update_country', id=1),
-            data={"country_name": "Testing update functionality"},
-            follow_redirects=True
-        )
-        self.assertIn(b"Testing update functionality", response.data)
+        with requests_mock.Mocker() as m:
+            m.get(f"http://{backend_host}/read/country/1", json=test_task)
+            m.put(f"http://{backend_host}/update/country/1", text="Test response")
+            test_country["country_name"] = "Testing update functionality"
+            m.get(f"http://{backend_host}/read/allCountries", json={ "country": [test_country] })
+            response = self.client.post(
+                url_for('update_country', id=1),
+                data={"country_name": "Testing update functionality"},
+                follow_redirects=True
+            )
+            self.assertIn(b"Testing update functionality", response.data)
     
-    # def test_visited_country(self):
-    #     response = self.client.get(url_for('visited_country', id=1), follow_redirects=True)
-    #     self.assertEqual(Country.query.get(1).visited, True)
+    def test_visited_country(self):
+        with requests_mock.Mocker() as m:
+            m.put(f"http://{backend_host}/visited/country/1")
+            test_country["visited"] = True
+            m.get(f"http://{backend_host}/read/allCountries", json={ "country": [test_country] })
+            response = self.client.get(url_for('visited_country', id=1), follow_redirects=True)
+            self.assert200(response)
     
-    # def test_unvisited_country(self):
-    #     response = self.client.get(url_for('unvisited_country', id=1), follow_redirects=True)
-    #     self.assertEqual(Country.query.get(1).visited, False)
+    def test_unvisited_country(self):
+        with requests_mock.Mocker() as m:
+            m.put(f"http://{backend_host}/unvisited_country/1")
+            test_country["visited"] = False
+            m.get(f"http://{backend_host}/read/allCountries", json={ "country": [test_country] })
+            response = self.client.get(url_for('unvisited_country', id=1), follow_redirects=True)
+            self.assert200(response)
         
 
 class TestDelete(TestBase):
 
     def test_delete_country(self):
-        response = self.client.get(
-            url_for('delete_country', id=1),
-            follow_redirects=True
-        )
-        self.assertNotIn(b"Run unit tests", response.data)
+        with requests_mock.Mocker() as m:
+            m.delete(f"http://{backend_host}/delete/country/1")
+            m.get(f"http://{backend_host}/read/allCountries", json={ "country": [] })
+            response = self.client.get(
+                url_for('delete_country', id=1),
+                follow_redirects=True
+            )
+            self.assertNotIn(b"Test the frontend", response.data)
